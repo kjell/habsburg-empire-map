340bb4242b8997401e938e63efa9ca4d90e723d1
Kjell <kjell@leanside.com>
2014-11-10 13:46:10 -0600

    Quick and dirty hapsburg's empire map
    Map tiles are proxied and cached from www.habsburger.net/en/map

---
+++ index.js
@@ -0,0 +1,29 @@
+var leaflet = require('leaflet')
+
+var centuries = [1300, 1400, 1500, 1600, 1700, 1800, 1900]
+  , tileUrl = "static.habsburger.net/assets/tiles/wdh-%d/{z}/{x}/{y}.png"
+  , layerUrls = centuries.map(function(year) { return "//10.1.4.95:3245/assets/tiles/wdh-"+year+"/{z}/{x}/{y}.png" })
+  , layers = layerUrls.map(function(url) { 
+      return L.tileLayer(url, {maxZoom: 6, minZoom: 5})
+    })
+
+var map = L.map('map').setView([47.197178, 13.73291], 5)
+
+layers.forEach(function(layer) { layer.addTo(map) })
+
+var activeLayer = layers[0];
+
+function update() {
+  var nextLayer = layers[(layers.indexOf(activeLayer)+1)%layers.length]
+  activeLayer = nextLayer
+  layers.forEach(function(layer) { layer.setZIndex(0) })
+  activeLayer.setZIndex(1)
+  setTimeout(update, 1000) 
+}
+update()
+
+window.api = {
+  map: map,
+  layers: layers,
+  update: update,
+}
