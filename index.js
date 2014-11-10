aae48e98c033ee3088de77d3a374d60ea54ab1b4
Kjell <kjell@leanside.com>
2014-11-10 13:52:08 -0600

    Rudimentary timescale

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ var map = L.map('map').setView([47.197178, 13.73291], 5)
 
 layers.forEach(function(layer) { layer.addTo(map) })
 
-var activeLayer = layers[0];
+var activeLayer = layers[0]
+
+var range = document.querySelector('input')
 
 function update() {
-  var nextLayer = layers[(layers.indexOf(activeLayer)+1)%layers.length]
+  var nextIndex = (layers.indexOf(activeLayer)+1)%layers.length
+    , nextLayer = layers[nextIndex]
   activeLayer = nextLayer
   layers.forEach(function(layer) { layer.setZIndex(0) })
   activeLayer.setZIndex(1)
+  range.value = nextIndex
   setTimeout(update, 1000) 
 }
 update()
